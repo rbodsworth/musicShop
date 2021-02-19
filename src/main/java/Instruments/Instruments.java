f10b11ad376cc84026ba2598e57d74fdef6f523a
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-19 14:54:18 +0000

    created instruments super class

--- src/main/java/Instruments/Instruments.java
+++ src/main/java/Instruments/Instruments.java
@@ -4,5 +4,23 @@ public abstract class Instruments {
 
     private String material;
     private String colour;
+    private InstrumentType instrumentType;
 
+    public Instruments(String material, String colour, InstrumentType instrumentType){
+        this.material = material;
+        this.colour = colour;
+        this.instrumentType = instrumentType;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public InstrumentType getInstrumentType() {
+        return instrumentType;
+    }
 }
