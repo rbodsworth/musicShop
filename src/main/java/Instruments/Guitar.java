05a65ac137613f047daf0f5cbaa0c2cd4956fe62
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-19 14:26:51 +0000

    guitar tests passing

--- src/main/java/Instruments/Guitar.java
+++ src/main/java/Instruments/Guitar.java
@@ -1,4 +1,32 @@
 package Instruments;
 
-public class Guitar {
+import Interfaces.IPlay;
+
+public class Guitar implements IPlay {
+
+    private String guitarBrandName;
+    private String guitarModelName;
+    private int numberOfGuitarStrings;
+
+    public Guitar(String guitarBrandName, String guitarModelName, int numberOfGuitarStrings){
+        this.guitarBrandName = guitarBrandName;
+        this.guitarModelName = guitarModelName;
+        this.numberOfGuitarStrings = numberOfGuitarStrings;
+    }
+
+    public String getGuitarBrandName() {
+        return guitarBrandName;
+    }
+
+    public String getGuitarModelName() {
+        return guitarModelName;
+    }
+
+    public int getNumberOfGuitarStrings() {
+        return numberOfGuitarStrings;
+    }
+
+    public String play() {
+        return "Sweet guitar riffs";
+    }
 }
--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -1,2 +1,32 @@
+import Instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("Fender", "Telecaster", 6);
+    }
+
+    @Test
+    public void guitarHasBrandName(){
+        assertEquals("Fender", guitar.getGuitarBrandName() );
+    }
+
+    @Test
+    public void guitarHasModelName(){
+        assertEquals("Telecaster", guitar.getGuitarModelName() );
+    }
+
+    @Test
+    public void guitarHasNumberOfStrings(){
+        assertEquals(6, guitar.getNumberOfGuitarStrings() );
+    }
+
+
 }
