e24cc6d2032600e8e719a0b4bfbae91e310fd599
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-19 14:35:32 +0000

    piano tests passing

--- src/main/java/Instruments/Piano.java
+++ src/main/java/Instruments/Piano.java
@@ -1,4 +1,20 @@
 package Instruments;
 
-public class Piano {
+import Interfaces.IPlay;
+
+public class Piano implements IPlay {
+
+    private int numberOfPianoKeys;
+
+    public Piano(int numberOfPianoKeys){
+        this.numberOfPianoKeys = numberOfPianoKeys;
+    }
+
+    public int getNumberOfPianoKeys() {
+        return numberOfPianoKeys;
+    }
+
+    public String play() {
+        return "plinky plonk";
+    }
 }
--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -1,2 +1,21 @@
+import Instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void before() { piano = new Piano(88);}
+
+    @Test
+    public void pianoHasNumberOfKeys(){
+        assertEquals(88, piano.getNumberOfPianoKeys());
+    }
 }
+
+
+
